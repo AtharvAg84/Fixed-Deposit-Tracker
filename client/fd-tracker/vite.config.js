886497e56fbe63253b834b34cf20bfe8f4407fa4
AtharvAg84 <atharv8484@gmail.com>
2026-02-18 00:07:30 +0530

    frontend vite config edit

--- client/fd-tracker/vite.config.js
+++ client/fd-tracker/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['fd-tracker-frontend.onrender.com']
+  }
 })
